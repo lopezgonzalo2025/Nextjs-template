38507d8699815b7099616cae630a648961c2e275
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-11-25 12:35:24 -0300

    chore: Prettier and Eslint config ⚙️

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  rules: {
+    semi: ['error', 'never'], // ;
+    'comma-dangle': ['error', 'never'], // [1,]
+    'no-unused-vars': [
+      'error',
+      {
+        // _ is a valid argument name for unused arguments in a function
+        argsIgnorePattern: '^_$'
+      }
+    ],
+    'max-len': ['error', 100],
+    'no-param-reassign': 'error',
+    'no-console': 'warn',
+    'no-alert': 'error',
+    quotes: ['error', 'single']
+  }
+}
