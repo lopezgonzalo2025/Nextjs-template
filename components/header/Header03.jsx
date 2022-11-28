40b9ab3bbf6f56b8346c2960e554f05856abd4f1
pmprete <pedro@thinkanddev.com>
2022-11-28 13:20:54 -0300

    Version 1.0.5 – November 29, 2022
    1) MetaMask Popup Modal Added
    2) Header Component Organized
    3) MetaMask Integration Issue Fixed
    4) Documentation Updated
    Version 1.0.4 – November 28, 2022
    
    1) Home Demo 10 (Crypto App) Added
    2) Documentation Updated
    Version 1.0.3 – November 27, 2022
    
    1) Home Demo 9 (DAO) Added
    2) Header Code Structure Improved
    3) Video Popup Modal Background AutoPlay Issue Fixed
    4) Documentation Updated
    Version 1.0.2 – October 22, 2022
    
    1) Home Demo 7 (Crypto Consultant) Added
    2) Home Demo 8 (NFT Game) Added
    3) New Inner Pages! Careers, Case Studies, Single Case Study Added
    4) Pages menu converted to 2 columns menu
    5) Added: Menu item "new" labels
    Version 1.0.1 – October 01, 2022
    
    1) Slider navigation button style mismatch issue fixed
    2) Hot bids section item spacing issue fixed
    3) Trending categories item height issue fixed
    4) Footer column width issue fixed
    New! FindHouse – Real Estate React NextJS Template
    vcamp creative agency react template
    
    Banu NFT Marketplace HTML Template
    
    Note
    1) While images and illustration are not in the download package – but if you need the images and illustration please feel free to contact with us.
    2) This Template is not WordPress Theme, and can’t be installed in WordPress.
    
    More items by ib-themes
    View author portfolio
    
    
    Overview
    
    Buy license
    Purchased licenses
    
    You have 1 license for this item. Download.
    Starting a new project? Buy another license.
    
    Support
    
    6 months left
    
    Need help with this item? Get support from ib-themes.
    
    How does support work?
    
    Rate the item
    
    ☆ ☆ ☆ ☆ ☆
    ib-themes
    ib-themes
    FreebieAuthor Level 6Featured AuthorExclusive AuthorFeatured Item1 Year of MembershipTrendsetterCollector Level 1
    View Portfolio
    Last Update	29 November 2022
    Published	30 August 2022
    High Resolution	Yes
    Compatible Browsers	IE11, Firefox, Safari, Opera, Chrome, Edge
    Compatible With	ReactJS
    ThemeForest Files Included	CSS Files, JS Files
    Columns	4+
    Documentation	Well Documented
    Layout	Responsive
    Tags	bitcoin, blockchain, creative, crypto collectibles, cryptocurrency, digital assets, ethereum, marketplace, metamask, nextjs, nft, non-fungible tokens, react, tailwind css, web3
    © All Rights Reserved ib-themes
    Contact the Envato Market Help Team

--- components/header/Header03.jsx
+++ components/header/Header03.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import Logo from "./../../public/images/logo.png";
 import WhiteLogo from "./../../public/images/logo_white.png";
 
@@ -10,7 +9,6 @@ export default function Header03() {
       <header className="js-page-header fixed top-0 z-20 w-full backdrop-blur transition-colors">
         <div className="container">
           <div className="flex items-center py-[1.5625rem] lg:py-[1.8125rem]">
-            {/* Logo */}
             <Link href="/">
               <a className="shrink-0 lg:mr-14">
                 <div className="max-h-7 dark:hidden">
@@ -31,9 +29,12 @@ export default function Header03() {
                 </div>
               </a>
             </Link>
+            {/* End logo */}
+
             <span className="font-display mt-1 hidden text-lg font-semibold lg:inline-block">
               Status
             </span>
+
             <a
               href="#"
               className="bg-accent shadow-accent-volume hover:bg-accent-dark ml-auto inline-block rounded-full py-2.5 px-8 text-center text-sm font-semibold text-white transition-all"
@@ -41,6 +42,7 @@ export default function Header03() {
               <span className="hidden lg:block">Subscribe to Updates</span>
               <span className="lg:hidden">Subscribe</span>
             </a>
+            {/* right content */}
           </div>
         </div>
       </header>
--- components/header/Header04.jsx
+++ components/header/Header04.jsx
@@ -396,6 +396,7 @@ export default function Header04() {
 
   return (
     <>
+      {/* main desktop menu sart*/}
       <header
         className={`js-page-header fixed top-0 z-20 w-full backdrop-blur transition-colors ${
           isScroll ? "js-page-header--is-sticky" : ""
@@ -423,6 +424,7 @@ export default function Header04() {
               </div>
             </a>
           </Link>
+          {/* End  logo */}
 
           <div className="js-mobile-menu dark:bg-jacarta-800 invisible fixed inset-0 z-10 ml-auto items-center bg-white opacity-0 lg:visible lg:relative lg:inset-auto lg:flex lg:bg-transparent lg:opacity-100 dark:lg:bg-transparent w-full">
             <nav className="navbar w-full mt-24  lg:mt-0">
@@ -625,19 +627,20 @@ export default function Header04() {
                 </li>
               </ul>
             </nav>
-            {/* End nav */}
+            {/* End menu for desktop */}
           </div>
-          {/* End center menu */}
+          {/* header menu conent end for desktop */}
 
           <div className="hidden  justify-end lg:flex">
             {/* <!-- CTA --> */}
-            <Link href="/collection/avatar_1">
+            <Link href="/collection/explore_collection">
               <a className="rounded-full bg-white py-3 px-8 text-center font-semibold text-accent shadow-white-volume transition-all hover:bg-accent-dark hover:text-white hover:shadow-accent-volume">
                 {" "}
                 Marketplace
               </a>
             </Link>
           </div>
+          {/* End right marketplace btn */}
 
           <div className="ml-auto flex justify-center lg:hidden">
             <button
@@ -660,8 +663,9 @@ export default function Header04() {
           {/* End mobile toggle button */}
         </div>
       </header>
-      {/* End desktop menu */}
+      {/* main desktop menu end */}
 
+      {/* start mobile menu and it's other materials  */}
       <div
         className={`lg:hidden js-mobile-menu dark:bg-jacarta-800 invisible fixed inset-0 z-20 ml-auto items-center bg-white opacity-0 lg:visible lg:relative lg:inset-auto lg:bg-transparent lg:opacity-100 dark:lg:bg-transparent ${
           toggle ? "nav-menu--is-open" : "hidden"
@@ -703,6 +707,7 @@ export default function Header04() {
             </svg>
           </button>
         </div>
+        {/* mobile menu top header content */}
 
         <form action="search" className="relative mt-24 mb-8 w-full lg:hidden">
           <input
@@ -723,7 +728,7 @@ export default function Header04() {
             </svg>
           </span>
         </form>
-        {/* End search form */}
+        {/* End search form mobile menu  */}
 
         <nav className="navbar w-full">
           <ul className="flex flex-col lg:flex-row">
@@ -959,7 +964,7 @@ export default function Header04() {
             </li>
           </ul>
         </nav>
-        {/* navbar */}
+        {/* End navbar mobile menu  */}
 
         <div className="mt-10 w-full lg:hidden">
           <div className="js-wallet bg-accent shadow-accent-volume hover:bg-accent-dark block w-full rounded-full py-3 px-8 text-center font-semibold text-white transition-all">
@@ -1040,140 +1045,8 @@ export default function Header04() {
           </div>
         </div>
         {/* mt-10 w-full lg:hidden */}
-
-        <div className="ml-8 hidden lg:flex xl:ml-12">
-          <div>
-            <button className="js-wallet border-jacarta-100 hover:bg-accent focus:bg-accent group dark:hover:bg-accent flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent dark:border-transparent dark:bg-white/[.15]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width={24}
-                height={24}
-                className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
-              >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path d="M22 6h-7a6 6 0 1 0 0 12h7v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2zm-7 2h8v8h-8a4 4 0 1 1 0-8zm0 3v2h3v-2h-3z" />
-              </svg>
-            </button>
-          </div>
-          <div className="js-nav-dropdown group-dropdown relative">
-            <button className="dropdown-toggle border-jacarta-100 hover:bg-accent focus:bg-accent group dark:hover:bg-accent ml-2 flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent dark:border-transparent dark:bg-white/[.15]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width={24}
-                height={24}
-                className="fill-jacarta-700 h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:fill-white"
-              >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z" />
-              </svg>
-            </button>
-            <div className="dropdown-menu dark:bg-jacarta-800 group-dropdown-hover:opacity-100 group-dropdown-hover:visible !-right-4 !top-[85%] !left-auto z-10 min-w-[14rem] whitespace-nowrap rounded-xl bg-white transition-all will-change-transform before:absolute before:-top-3 before:h-3 before:w-full lg:absolute lg:grid lg:!translate-y-4 lg:py-4 lg:px-2 lg:shadow-2xl hidden lg:invisible lg:opacity-0">
-              <div>
-                <button className="js-copy-clipboard font-display text-jacarta-700 my-4 flex select-none items-center whitespace-nowrap px-5 leading-none dark:text-white">
-                  <span>0x7a86c0b06417100...</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width={24}
-                    height={24}
-                    className="dark:fill-jacarta-300 fill-jacarta-500 ml-auto mb-px h-4 w-4"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M7 7V3a1 1 0 0 1 1-1h13a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-4v3.993c0 .556-.449 1.007-1.007 1.007H3.007A1.006 1.006 0 0 1 2 20.993l.003-12.986C2.003 7.451 2.452 7 3.01 7H7zm2 0h6.993C16.549 7 17 7.449 17 8.007V15h3V4H9v3zM4.003 9L4 20h11V9H4.003z" />
-                  </svg>
-                </button>
-              </div>
-              <div className="dark:border-jacarta-600 border-jacarta-100 mx-5 mb-6 rounded-lg border p-4">
-                <span className="dark:text-jacarta-200 text-sm font-medium tracking-tight">
-                  Balance
-                </span>
-                <div className="flex items-center">
-                  <svg className="icon icon-ETH -ml-1 mr-1 h-[1.125rem] w-[1.125rem]">
-                    <use xlinkHref="/icons.svg#icon-ETH" />
-                  </svg>
-                  <span className="text-green text-lg font-bold">10 ETH</span>
-                </div>
-              </div>
-              <Link href="/user/avatar_6">
-                <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width={24}
-                    height={24}
-                    className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z" />
-                  </svg>
-                  <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
-                    My Profile
-                  </span>
-                </a>
-              </Link>
-              <Link href="/profile/user_avatar">
-                <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width={24}
-                    height={24}
-                    className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M9.954 2.21a9.99 9.99 0 0 1 4.091-.002A3.993 3.993 0 0 0 16 5.07a3.993 3.993 0 0 0 3.457.261A9.99 9.99 0 0 1 21.5 8.876 3.993 3.993 0 0 0 20 12c0 1.264.586 2.391 1.502 3.124a10.043 10.043 0 0 1-2.046 3.543 3.993 3.993 0 0 0-3.456.261 3.993 3.993 0 0 0-1.954 2.86 9.99 9.99 0 0 1-4.091.004A3.993 3.993 0 0 0 8 18.927a3.993 3.993 0 0 0-3.457-.26A9.99 9.99 0 0 1 2.5 15.121 3.993 3.993 0 0 0 4 11.999a3.993 3.993 0 0 0-1.502-3.124 10.043 10.043 0 0 1 2.046-3.543A3.993 3.993 0 0 0 8 5.071a3.993 3.993 0 0 0 1.954-2.86zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                  </svg>
-                  <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
-                    Edit Profile
-                  </span>
-                </a>
-              </Link>
-              <Link href="/login">
-                <a className="dark:hover:bg-jacarta-600 hover:text-accent focus:text-accent hover:bg-jacarta-50 flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width={24}
-                    height={24}
-                    className="fill-jacarta-700 h-4 w-4 transition-colors dark:fill-white"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM7 11V8l-5 4 5 4v-3h8v-2H7z" />
-                  </svg>
-                  <span className="font-display text-jacarta-700 mt-1 text-sm dark:text-white">
-                    Sign out
-                  </span>
-                </a>
-              </Link>
-            </div>
-          </div>
-          <button className="border-jacarta-100 hover:bg-accent focus:bg-accent group dark:hover:bg-accent js-dark-mode-trigger ml-2 flex h-10 w-10 items-center justify-center rounded-full border bg-white transition-colors hover:border-transparent focus:border-transparent dark:border-transparent dark:bg-white/[.15]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width={24}
-              height={24}
-              className="fill-jacarta-700 dark-mode-light h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:hidden"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width={24}
-              height={24}
-              className="fill-jacarta-700 dark-mode-dark hidden h-4 w-4 transition-colors group-hover:fill-white group-focus:fill-white dark:block dark:fill-white"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z" />
-            </svg>
-          </button>
-        </div>
       </div>
-      {/* End mobile menu */}
+      {/* End mobile menu and it's other materials */}
     </>
   );
 }
