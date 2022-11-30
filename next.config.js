bcaad259e7e7cad81de0d0f7e188c2a73b25a47a
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-11-30 10:44:20 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  productionBrowserSourceMaps: true,
+  // reactStrictMode: true,
+  // productionBrowserSourceMaps: true,
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/dk8s7xjsl/image/upload/'
