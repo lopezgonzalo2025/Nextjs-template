37abdfd308b45a0780754137978f0c45b54af171
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-11-30 13:26:20 -0300

    chore: config github actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: true
+  productionBrowserSourceMaps: true,
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/dk8s7xjsl/image/upload/'
+  }
 }
 
 module.exports = nextConfig
