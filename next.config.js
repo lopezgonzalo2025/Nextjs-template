d850411cc7c8057b1f73e5026f397c9fb9566140
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-11-30 10:48:54 -0300

    next js config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // reactStrictMode: true,
-  // productionBrowserSourceMaps: true,
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  target: 'serverless',
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/dk8s7xjsl/image/upload/'
   }
-}
-
-module.exports = nextConfig
+})
