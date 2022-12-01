f75cb5c158f6bcc69ee9d58e7c4cae36697478ce
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2022-12-01 00:11:24 -0300

    test: Jest config 🧪

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -65,3 +65,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
